e8196921263faf25debf70e5bca73d1602c51c58
Dmitri Pisarev <dimaip@gmail.com>
2016-10-26 15:00:59 +0300

    Init

---
+++ Classes/Psmb/MapReduce/Eel/FlowQueryOperations/MapOperation.php
@@ -0,0 +1,75 @@
+<?php
+namespace Psmb\MapReduce\Eel\FlowQueryOperations;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Eel\FlowQuery\Operations\AbstractOperation;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+
+/**
+ * Map Operation
+ *
+ * Takes an Eel Expression as a first argument and maps it to every context variable.
+ * Current value is injected as `value` context variable.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ */
+class MapOperation extends AbstractOperation {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var string
+	 */
+	static protected $shortName = 'map';
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var integer
+	 */
+	static protected $priority = 100;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @var boolean
+	 */
+	static protected $final = TRUE;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param array (or array-like object) $context onto which this operation should be applied
+	 * @return boolean TRUE if the operation can be applied onto the $context, FALSE otherwise
+	 */
+	public function canEvaluate($context) {
+		return isset($context[0]);
+	}
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Eel\EelEvaluatorInterface
+	 */
+	protected $eelEvaluator;
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param \TYPO3\Eel\FlowQuery\FlowQuery $flowQuery
+	 * @param array $arguments
+	 * @return void
+	 */
+	public function evaluate(\TYPO3\Eel\FlowQuery\FlowQuery $flowQuery, array $arguments) {
+		if (!isset($arguments[0]) || empty($arguments[0])) {
+			throw new \TYPO3\Eel\FlowQuery\FlowQueryException('No Eel expression provided', 1332492243);
+		}
+		$expression = '${' . $arguments[0] . '}';
+		$context = $flowQuery->getContext();
+
+		return array_map(
+			function ($value) use ($expression, $context) {
+				return \TYPO3\Eel\Utility::evaluateEelExpression($expression, $this->eelEvaluator, array('value' => $value));
+			},
+			$context
+		);
+	}
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# Psmb.MapReduce
+
+This package provides `.map()` and `.reduce()` FlowQuery operations.
+Could be useful, huh?
+
+## Usage
+
+### Map
+
+Takes all items in current FlowQuery context, and transforms each value with given Eel operation.
+There is a context variable `value` available, with a value of a current node.
+
+E.g. this would give you an array of identifiers of all child nodes of a given node:
+```
+${q(node).children().map('value.identifier')}
+```
+
+See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+### Reduce
+
+Takes an Eel Expression as a first argument and initial values as the second. Injects `previousValue`,
+`currentValue`, `index` and `array` context variables.
+
+E.g. imagine you have a collection of Order nodes, where each node has a price property.
+Now let's try to get a total price for all of the give nodes:
+
+```
+${q(node).children('orders').reduce('previousValue + currentValue.properties.price', 0)
+```
+
+See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
